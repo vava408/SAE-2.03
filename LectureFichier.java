96cf736d586e4b4323a299eef863a01ef62daec2
CookIsLife <fabien.stratmains@gmail.com>
2025-12-08 10:07:48 +0100

    création fonction lecture

--- LectureFichier.java
+++ LectureFichier.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
 public class LectureFichier
 {
-	
+	public static void main(String[] arg)
+	{
+		try
+		{
+			Scanner sc = new Scanner ( new FileInputStream ( arg[0] ) );
+
+			while ( sc.hasNextLine() )
+			{
+				System.out.println ( sc.nextLine() );
+			}
+
+			sc.close();
+		}
+		catch (Exception e){ e.printStackTrace(); }
+	}
 }

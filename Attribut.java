8f7adb1951375efa0b0d3e8a6a70f0aa932daefe
Valentin Lemaitre <valentinlemaitre06@gmail.com>
2025-12-08 10:53:33 +0100

    Correction du nom attribut

---
+++ Attribut.java
@@ -0,0 +1,48 @@
+public class Attribut
+{
+	private int attributId;
+	private String nom;
+	private String type;
+	private String visibilite;
+	private String instance;
+
+	public Attribut(int attributId, String nom, String type, String visibilite, String instance)
+	{
+		this.attributId = attributId;
+		this.nom = nom;
+		this.type = type;
+		this.visibilite = visibilite;
+		this.instance = instance;
+	}
+
+	public int getAttributId()
+	{
+		return attributId;
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public String getVisibilite()
+	{
+		return visibilite;
+	}
+
+	public String getInstance()
+	{
+		return instance;
+	}
+
+	public String toString()
+	{
+		return "Attribut [attributId=" + attributId + ", nom=" + nom + ", type=" + type + ", visibilite=" + visibilite
+				+ ", instance=" + instance + "]";
+	}
+}

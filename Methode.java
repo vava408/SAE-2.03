b501a4396109f932b370ea0357ca01566cd9c5cb
Floriane Lepiller <flostiv708@gmail.com>
2025-12-08 10:45:23 +0100

    création de la classe méthode

--- Methode.java
+++ Methode.java
@@ -1,4 +1,63 @@
-public class Methode
+public class Methode 
 {
+    private String nom;
+    private String visibilite;
+    private String retour;
+	private String parametre;
+
+
+	public Methode(String nom, String visibilite, String retour, String parametre)
+	{
+		this.nom = nom;
+		this.visibilite = visibilite;
+		this.retour = retour;
+		this.parametre = parametre;
+	}
+
+	public String getNom() 
+	{
+		return nom;
+	}
+
+	public String getVisibilite() 
+	{
+		return visibilite;
+	}
+
+
+	public String getRetour() 
+	{
+		return retour;
+	}
+
+
+	public String getParametre() 
+	{
+		return parametre;
+	}
+
+	public void setNom(String nom) 
+	{
+		this.nom = nom;
+	}
+
+	public void setVisibilite(String visibilite) 
+	{
+		this.visibilite = visibilite;
+	}
+
+	public void setRetour(String retour) 
+	{
+		this.retour = retour;
+	}
+
+	public void setParametre(String parametre) 
+	{
+		this.parametre = parametre;
+	}
+
+	
+
+
 
 }

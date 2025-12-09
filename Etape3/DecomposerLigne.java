8d0be5ae4ef5b4875c45ca31b13e86839d358a97
CookIsLife <fabien.stratmains@gmail.com>
2025-12-09 16:06:06 +0100

    rajout des points virgules pour le traitement

--- Etape3/DecomposerLigne.java
+++ Etape3/DecomposerLigne.java
@@ -3,13 +3,6 @@ import java.util.ArrayList;
 
 public class DecomposerLigne
 {
-    private LireFichier lireFichier;
-
-    public DecomposerLigne( LireFichier lireFichier )
-    {
-        this.lireFichier = lireFichier;
-    }
-
     //méthode pour retirer une ligne de tout ce qui n'est pas utile pour la rendre utilisable
     //puis de la décomposer à l'aide d'un scanner pour rentrer chaque valeurs dans un tableau
     public String[] decomposerLigne(String ligne)
@@ -59,10 +52,6 @@ public class DecomposerLigne
                 if(s.startsWith("\""))
                     s = null;
 
-                //on quitte la boucle si on rencontre un point virgule qui est toujours à la fin d'un attribut
-                if(s.startsWith(";"))
-                    break;
-
                 //quitte la boucle si on rencontre throws qui est toujours à la fin d'une méthode
                 if(s.startsWith("throws"))
                     break;
@@ -83,7 +72,7 @@ public class DecomposerLigne
             //transfert chaque mot de la liste dans le tableau
             for(int i = 0; i < lstMots.size(); i++)
                 tabRet[i] = lstMots.get(i);
-            
+
             return tabRet;
     }
 }

dd3f8ce866f637a169fe757ad59f1554ed2566cd
Valentin Lemaitre <valentinlemaitre06@gmail.com>
2025-12-09 14:05:06 +0100

    Ajout des fichier

---
+++ Etape4/Point.java
@@ -0,0 +1,33 @@
+public class Point extends Object
+{
+    private String nom;
+    private int    x;
+    private int    y;
+
+    public Point( String nom, int x, int y ) 
+    {
+        this.nom = nom;
+        this.x   = x;
+        this.y   = y;
+    }
+
+    public int getX() 
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setX( int x ) 
+    {
+        this.x = x;
+    }
+
+    public void setY( int y ) 
+    {
+        this.y = y;
+    }
+}

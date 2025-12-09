f9afc8fd245054159dc87a075589479ac58feb41
Floriane Lepiller <flostiv708@gmail.com>
2025-12-09 08:42:01 +0100

    Création du repertoire étape 1

---
+++ Etape1/Parametre.java
@@ -0,0 +1,53 @@
+public class Parametre 
+{
+    private static int nbParametre = 0;
+	private int        id;
+	private String     nom;
+    private String     type;
+
+    public Parametre(String nom, String type) 
+	{
+		this.id   = ++nbParametre;
+		this.nom  = nom;
+        this.type = type;
+    }
+
+    public String getNom() 
+	{
+        return this.nom;
+    }
+
+    public String getType() 
+	{
+        return this.type;
+    }
+
+	public int getId() 
+	{
+        return this.id;
+    }
+
+    public void setNom(String nom) 
+	{
+        this.nom = nom;
+    }
+
+    public void setType(String type) 
+	{
+        this.type = type;
+    }
+
+	public int setId() 
+	{
+        return this.id;
+    }
+
+	public String toString() 
+	{
+        String sb = "";
+        sb += "p" + getId() + " :" + getNom();
+        sb += " type : " + getType();
+        return sb;
+    }
+
+}

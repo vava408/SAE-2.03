ff3aedebe0808d0e7571eda1200ad3784cf84c37
Maxence Leheurteur <maxence.leheurteur@gmail.com>
2025-12-09 08:50:59 +0100

    ADD/Folder Etape2

---
+++ Etape2/Point.java
@@ -0,0 +1,33 @@
+public class Point 
+{
+    private String nom;
+    private int    x;
+    private int    y;
+
+    public Point( String nom, int x, int y ) 
+    {
+        this.nom = nom;
+        this.x   = x;
+        this.y   = y;
+    }
+
+    public int getX() 
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setX( int x ) 
+    {
+        this.x = x;
+    }
+
+    public void setY( int y ) 
+    {
+        this.y = y;
+    }
+}

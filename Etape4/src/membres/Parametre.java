db42b518d6ee45a46a2584e5a21ce40449ba320e
Floriane Lepiller <flostiv708@gmail.com>
2025-12-11 13:30:58 +0100

    Correction des commentaires de membre de l'étape 4

--- Etape4/src/membres/Parametre.java
+++ Etape4/src/membres/Parametre.java
@@ -2,52 +2,52 @@ package src.membres;
 
 /*-------------------------------------------------------------------*/
 /*- Classe Parametre : Représente un paramètre d'une méthode UML     */
-/*- Etape 1                                                          */
+/*- Etape 4                                                          */
 /*- Groupe 6                                                         */
-/*- Date de création : 08/12/2025 10:30                              */
+/*- Date de création : 09/12/2025 11:50                              */
 /*-------------------------------------------------------------------*/
 
 public class Parametre 
 {
-    /*--------------------------------------------------------------*/
-    /* Déclaration des attributs                                    */
-    /*--------------------------------------------------------------*/
-    private int    id;
-    private String nom;
-    private String type;
+	/*--------------------------------------------------------------*/
+	/* Déclaration des attributs                                    */
+	/*--------------------------------------------------------------*/
+	private int    id;
+	private String nom;
+	private String type;
 
-    /*--------------------------------------------------------------*/
-    /* Constructeur : initialise un paramètre                       */
-    /*--------------------------------------------------------------*/
-    public Parametre(int numParametre, String nom, String type) 
-    {
-        this.id   = numParametre;
-        this.nom  = nom;
-        this.type = type;
-    }
+	/*--------------------------------------------------------------*/
+	/* Constructeur : initialise un paramètre                       */
+	/*--------------------------------------------------------------*/
+	public Parametre(int numParametre, String nom, String type) 
+	{
+		this.id   = numParametre;
+		this.nom  = nom;
+		this.type = type;
+	}
 
-    /*--------------------------------------------------------------*/
-    /* Accesseurs : retourne les attributs de la classe            */
-    /*--------------------------------------------------------------*/
-    public int    getId  () { return this.id   ;}
-    public String getNom () { return this.nom  ;}
-    public String getType() { return this.type ;}
+	/*--------------------------------------------------------------*/
+	/* Accesseurs : retourne les attributs de la classe            */
+	/*--------------------------------------------------------------*/
+	public int    getId  () { return this.id   ;}
+	public String getNom () { return this.nom  ;}
+	public String getType() { return this.type ;}
 
-    /*--------------------------------------------------------------*/
-    /* Modificateurs : modifie les attributs de la classe          */
-    /*--------------------------------------------------------------*/
-    public void setId  (int id)      { this.id   = id   ;}
-    public void setNom (String nom)  { this.nom  = nom  ;}
-    public void setType(String type) { this.type = type ;}
+	/*--------------------------------------------------------------*/
+	/* Modificateurs : modifie les attributs de la classe          */
+	/*--------------------------------------------------------------*/
+	public void setId  (int id)      { this.id   = id   ;}
+	public void setNom (String nom)  { this.nom  = nom  ;}
+	public void setType(String type) { this.type = type ;}
 
-    /*--------------------------------------------------------------*/
-    /* Retourne une chaîne décrivant le paramètre                  */
-    /*--------------------------------------------------------------*/
-    public String toString() 
-    {
-        String sRet = "";
-        sRet += "p" + this.getId() + " :" + this.getNom();
-        sRet += " type : " + this.getType();
-        return sRet;
-    }
+	/*--------------------------------------------------------------*/
+	/* Retourne une chaîne décrivant le paramètre                  */
+	/*--------------------------------------------------------------*/
+	public String toString() 
+	{
+		String sRet = "";
+		sRet += "p" + this.getId() + " :" + this.getNom();
+		sRet += " type : " + this.getType();
+		return sRet;
+	}
 }

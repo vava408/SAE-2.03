55e827d7c2e15190a18b6833440a0f4a9600af64
Floriane Lepiller <flostiv708@gmail.com>
2025-12-16 15:30:51 +0100

    Pointe de fleche

--- Etape5/src/membres/Association.java
+++ Etape5/src/membres/Association.java
@@ -6,8 +6,8 @@ public class Association implements Serializable
 {
 	private String nomClasseA   ;
 	private String nomClasseB   ;
-    private String multiplicityA;
-	private String multiplicityB;
+	private String multipliciteA;
+	private String multipliciteB;
 
 	// Constructeur
 	public Association( String nomClasseA, String nomClasseB, String multiplicityA, String multiplicityB )
@@ -15,42 +15,48 @@ public class Association implements Serializable
 		this.nomClasseA    = nomClasseA;
 		this.nomClasseB    = nomClasseB;
 
-		this.multiplicityA = multiplicityA;
-		this.multiplicityB = multiplicityB;
+		this.multipliciteA = multiplicityA;
+		this.multipliciteB = multiplicityB;
 	}
 
 	public String getNomClasseA   () { return this.nomClasseA   ; }
 	public String getNomClasseB   () { return this.nomClasseB   ; }
 
-	public String getMultiplicityA() { return this.multiplicityA; }
-	public String getMultiplicityB() { return this.multiplicityB; }
+	public String getMultiplicityA() { return this.multipliciteA; }
+	public String getMultiplicityB() { return this.multipliciteB; }
+
+	public boolean estUnidirectionnelle() 
+	{
+		return (this.multipliciteA.equals("0..*") && this.multipliciteB.equals("1..1")) ||
+		   	   (this.multipliciteB.equals("0..*") && this.multipliciteA.equals("1..1"));
+	}
 
 	public String toString() 
 	{
 		String sRet = "";
 
-		if ( this.multiplicityA.equals( "0..*" ) && this.multiplicityB.equals( "1..1" ) ||
-	         this.multiplicityB.equals( "0..*" ) && this.multiplicityA.equals( "1..1" )    )
+		if ( this.multipliciteA.equals( "0..*" ) && this.multipliciteB.equals( "1..1" ) ||
+			 this.multipliciteB.equals( "0..*" ) && this.multipliciteA.equals( "1..1" )    )
 		{
 			sRet += "Unidirectionnelle de ";
 
-			if ( this.multiplicityB.equals( "1..1" ) )
+			if ( this.multipliciteB.equals( "1..1" ) )
 			{
-				sRet += this.nomClasseA + "(" + this.multiplicityA + ") vers " + 
-			            this.nomClasseB + "(" + this.multiplicityB + ")";
+				sRet += this.nomClasseA + "(" + this.multipliciteA + ") vers " + 
+						this.nomClasseB + "(" + this.multipliciteB + ")";
 			}
 			else
 			{
-				sRet += this.nomClasseB + "(" + this.multiplicityB + ") vers " + 
-			            this.nomClasseA + "(" + this.multiplicityA + ")";
+				sRet += this.nomClasseB + "(" + this.multipliciteB + ") vers " + 
+						this.nomClasseA + "(" + this.multipliciteA + ")";
 			}
 		}
 		else
 		{
-			sRet += "Bidirectionnelle entre " + this.nomClasseA + "(" + this.multiplicityA + ") et " + 
-			                                    this.nomClasseB + "(" + this.multiplicityB + ")";
+			sRet += "Bidirectionnelle entre " + this.nomClasseA + "(" + this.multipliciteA + ") et " + 
+												this.nomClasseB + "(" + this.multipliciteB + ")";
 		}
 
 		return sRet;	
-    }
+	}
 }

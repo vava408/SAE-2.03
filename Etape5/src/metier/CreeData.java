2183187f5f2123a4c090728ef7bd962d5fcd24b1
Valentin Lemaitre <valentinlemaitre06@gmail.com>
2025-12-17 09:05:28 +0100

    Ajout import

--- Etape5/src/metier/CreeData.java
+++ Etape5/src/metier/CreeData.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import src.membres.Association;
 import src.membres.Attribut;
 import src.membres.Methode;
 

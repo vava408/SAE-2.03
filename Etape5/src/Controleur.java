96d436e907b69df8e47ed7ed2aba2dcd98b14acc
Floriane Lepiller <flostiv708@gmail.com>
2025-12-19 11:00:14 +0100

    Commentaire + indentation de lire Bloc et controlleur

--- Etape5/src/Controleur.java
+++ Etape5/src/Controleur.java
@@ -7,114 +7,222 @@ import src.ihm.Vue;
 import src.membres.Association;
 import src.membres.Attribut;
 import src.membres.Methode;
+import src.metier.LireData;
 import src.metier.LireDossier;
 import src.metier.LireFichier;
 import src.metier.LireSauvegarde;
 import src.metier.Sauvegarder;
-import src.metier.LireData;
 
+/**
+ * Contrôleur principal de l'application de génération de diagrammes UML.
+ * Fait le lien entre l'interface graphique (Vue, FrameUML) et la logique métier
+ * (lecture de fichiers, sauvegarde, chargement).
+ * 
+ * Exercice : Générateur de diagrammes UML
+ * Groupe : 6
+ * Auteurs : Groupe 6
+ * Date de création : 10/12/2025 16:00
+ */
 public class Controleur
 {
-    LireDossier lireDossier;
-    FrameUML    frameUML   ;
-    Vue         vue        ;
-    Sauvegarder sauvegarder;
-    LireSauvegarde     charger    ;
+	/*--------------------------------------------------------------*/
+	/* Déclaration des attributs                                    */
+	/*--------------------------------------------------------------*/
 
+	private LireDossier    lireDossier;
+	private FrameUML       frameUML;
+	private Vue            vue;
+	private Sauvegarder    sauvegarder;
+	private LireSauvegarde charger;
 
-    public Controleur()
-    {
-        this.vue         = new Vue     ( this );
-        this.frameUML    = new FrameUML( this );
-        this.sauvegarder = new Sauvegarder();
-        this.charger     = new LireSauvegarde();
+	/*--------------------------------------------------------------*/
+	/* Constructeur                                                 */
+	/*--------------------------------------------------------------*/
 
-    }
+	/**
+	* Construit le contrôleur et initialise l'interface graphique.
+	*/
+	public Controleur()
+	{
+		this.vue         = new Vue( this );
+		this.frameUML    = new FrameUML( this );
+		this.sauvegarder = new Sauvegarder();
+		this.charger     = new LireSauvegarde();
+	}
 
-    public void lireDossier( String chemin )
-    {
-        this.lireDossier = new LireDossier( this, chemin );
-    }
+	/*--------------------------------------------------------------*/
+	/* Modificateurs                                                */
+	/*--------------------------------------------------------------*/
 
-    public void lireData(String chemin)
-    {
-        LireData data = new LireData(chemin, this);
-    }
+	/**
+	* Définit la position d'une classe dans le diagramme.
+	* @param lF le fichier représentant la classe
+	* @param x position horizontale
+	* @param y position verticale
+	*/
+	public void setPosition( LireFichier lF, int x, int y )
+	{
+		this.lireDossier.setPosition( lF, x, y );
+	}
 
-    public LireDossier getLireDossier()
-    {
-        return this.lireDossier;
-    }
+	/**
+	* Définit le dossier de lecture des fichiers.
+	* @param dossier le nouveau dossier à utiliser
+	*/
+	public void setLireDossier( LireDossier dossier )
+	{
+		this.lireDossier = dossier;
+	}
 
-    public FrameUML getFrameUML()
-    {
-        return this.frameUML;
-    }
+	/*--------------------------------------------------------------*/
+	/* Accesseurs                                                   */
+	/*--------------------------------------------------------------*/
 
-    public Vue getVue()
-    {
-        return this.vue;
-    }
+	/**
+	* Retourne l'objet de lecture du dossier.
+	* @return le lecteur de dossier
+	*/
+	public LireDossier getLireDossier()
+	{
+		return this.lireDossier;
+	}
 
-    public int getNbClasses()
-    {
-        return this.lireDossier.getNbClasses();
-    }
+	/**
+	* Retourne la fenêtre principale de l'application.
+	* @return la frame UML
+	*/
+	public FrameUML getFrameUML()
+	{
+		return this.frameUML;
+	}
 
-    public ArrayList<LireFichier> getListeFichiers()
-    {
-        return this.lireDossier.getListeFichiers();
-    }
+	/**
+	* Retourne la vue textuelle de l'application.
+	* @return la vue
+	*/
+	public Vue getVue()
+	{
+		return this.vue;
+	}
 
-    public ArrayList<Association> getListeAssociation()
-    {
-        return this.lireDossier.getListeAssociation();
-    }
+	/**
+	* Retourne le nombre de classes chargées.
+	* @return nombre de classes
+	*/
+	public int getNbClasses()
+	{
+		return this.lireDossier.getNbClasses();
+	}
 
+	/**
+	* Retourne la liste des fichiers analysés.
+	* @return liste des fichiers
+	*/
+	public ArrayList<LireFichier> getListeFichiers()
+	{
+		return this.lireDossier.getListeFichiers();
+	}
+
+	/**
+	* Retourne la liste des associations entre classes.
+	* @return liste des associations
+	*/
+	public ArrayList<Association> getListeAssociation()
+	{
+		return this.lireDossier.getListeAssociation();
+	}
+
+	/**
+	* Retourne la map des attributs d'associations.
+	* @return map des attributs d'associations
+	*/
 	public HashMap<Association, ArrayList<String>> gethMAttributsAssociations()
 	{
 		return this.lireDossier.gethMAttributsAssociations();
 	}
 
-    public void setPosition( LireFichier lF, int x, int y )
-    {
-        this.lireDossier.setPosition( lF, x, y );
-    }
+	/*--------------------------------------------------------------*/
+	/* Autres méthodes                                              */
+	/*--------------------------------------------------------------*/
 
-    public String afficherAttribut( Attribut a )
-    {
-        return this.vue.afficherAttribut( a );
-    }
-
-    public String afficherMethode( Methode m, boolean complet )
-    {
-        return this.vue.afficherMethode( m, complet );
-    }
-
-    public void sauvegarder()
-    {
-        this.sauvegarder.sauvegarder( lireDossier );
-    }
-
-    public void charger(String path)
-    {
-        this.charger.charger(this, path);
-    }
-
-    public boolean nomEstDansRepertoire( String nomClasse )
-    {
-        return this.lireDossier.nomEstDansRepertoire( nomClasse );
-    }
-
-	public void setLireDossier(LireDossier dossier)
+	/**
+	* Lit et analyse un dossier contenant des fichiers Java.
+	* @param chemin chemin du dossier à lire
+	*/
+	public void lireDossier( String chemin )
 	{
-		this.lireDossier = dossier;
+		this.lireDossier = new LireDossier( this, chemin );
 	}
 
-    public static void main ( String[] args )
-    {
-        new Controleur();
-    }
+	/**
+	* Charge un fichier de données sérialisé.
+	* @param chemin chemin du fichier à charger
+	*/
+	public void lireData( String chemin )
+	{
+		LireData data;
+		
+		data = new LireData( chemin, this );
+	}
 
+	/**
+	* Formate l'affichage d'un attribut.
+	* @param a l'attribut à afficher
+	* @return chaîne formatée de l'attribut
+	*/
+	public String afficherAttribut( Attribut a )
+	{
+		return this.vue.afficherAttribut( a );
+	}
 
+	/**
+	* Formate l'affichage d'une méthode.
+	* @param m la méthode à afficher
+	* @param complet si true, affiche tous les paramètres
+	* @return chaîne formatée de la méthode
+	*/
+	public String afficherMethode( Methode m, boolean complet )
+	{
+		return this.vue.afficherMethode( m, complet );
+	}
+
+	/**
+	* Sauvegarde l'état actuel du diagramme.
+	*/
+	public void sauvegarder()
+	{
+		this.sauvegarder.sauvegarder( this.lireDossier );
+	}
+
+	/**
+	* Charge un fichier de sauvegarde.
+	* @param path chemin du fichier à charger
+	*/
+	public void charger( String path )
+	{
+		this.charger.charger( this, path );
+	}
+
+	/**
+	* Vérifie si un nom de classe existe dans le répertoire chargé.
+	* @param nomClasse nom de la classe à vérifier
+	* @return true si la classe existe, false sinon
+	*/
+	public boolean nomEstDansRepertoire( String nomClasse )
+	{
+		return this.lireDossier.nomEstDansRepertoire( nomClasse );
+	}
+
+	/*--------------------------------------------------------------*/
+	/* Méthode principale                                           */
+	/*--------------------------------------------------------------*/
+
+	/**
+	* Point d'entrée de l'application.
+	* @param args arguments de ligne de commande (non utilisés)
+	*/
+	public static void main( String[] args )
+	{
+		new Controleur();
+	}
 }
--- Etape5/src/ihm/Bloc.java
+++ Etape5/src/ihm/Bloc.java
@@ -7,112 +7,160 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.*;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
-
 import src.membres.Attribut;
 import src.membres.Methode;
 
+/**
+ * Représente un bloc visuel d'une classe UML dans le diagramme.
+ * Affiche le nom de la classe, ses attributs et méthodes. Gère le drag-and-drop
+ * et l'affichage complet/réduit selon l'interaction de l'utilisateur.
+ * 
+ * Exercice : Générateur de diagrammes UML
+ * Groupe : 6
+ * Auteurs : Groupe 6
+ * Date de création : 10/12/2025 15:30
+ */
 public class Bloc extends JPanel
 {
-	private PanelPrincipal panelPrincipal;
+	/*--------------------------------------------------------------*/
+	/* Déclaration des attributs                                    */
+	/*--------------------------------------------------------------*/
 
-	private String         nomClasse;
+	private PanelPrincipal  panelPrincipal;
+	private String          nomClasse;
+	private int             largeurMax;
+	private int             hauteurMax;
+	private boolean         estClique;
 
-	private int            largeurMax;
-	private int            hauteurMax;
+	/*--------------------------------------------------------------*/
+	/* Constructeur                                                 */
+	/*--------------------------------------------------------------*/
 
-	//booleen pour savoir si on clique sur le bloc
-	private boolean estClique;
+	/**
+	* Construit un bloc représentant une classe UML.
+	* @param panelPrincipal le panel parent contenant ce bloc
+	* @param nomClasse nom de la classe à afficher
+	*/
+	public Bloc( PanelPrincipal panelPrincipal, String nomClasse )
+	{
+		GereSouris gs;
 
-    public Bloc ( PanelPrincipal panelPrincipal, String nomClasse )
-    {
-		this.estClique = false;
+		this.estClique      = false;
+		this.panelPrincipal = panelPrincipal;
+		this.nomClasse      = nomClasse;
+		this.largeurMax     = 0;
+		this.hauteurMax     = 0;
 
-        this.panelPrincipal = panelPrincipal;
-		
-		this.nomClasse = nomClasse;
-		
-		this.largeurMax = 0;
-		this.hauteurMax = 0;
-		
-        this.setBackground ( new Color ( 250, 250, 250 ) );
-		
-        GereSouris gs = new GereSouris ( );
-        this.addMouseListener ( gs );
-        this.addMouseMotionListener ( gs );
-    }
+		this.setBackground( new Color( 250, 250, 250 ) );
 
+		gs = new GereSouris();
+		this.addMouseListener( gs );
+		this.addMouseMotionListener( gs );
+	}
+
+	/*--------------------------------------------------------------*/
+	/* Accesseurs                                                   */
+	/*--------------------------------------------------------------*/
+
+	/**
+		* Retourne le nom de la classe représentée par ce bloc.
+		* @return nom de la classe
+		*/
 	public String getName()
 	{
 		return this.nomClasse;
 	}
 
-    public void maj()
+	/*--------------------------------------------------------------*/
+	/* Autres méthodes                                              */
+	/*--------------------------------------------------------------*/
+
+	/**
+	* Met à jour les dimensions du bloc selon son contenu et son état.
+	*/
+	public void maj()
 	{
-		//on récupère la hauteur et la largeur des blocs
+		/* Calcul de la hauteur et largeur selon le contenu */
 		if ( this.panelPrincipal.nomEstDansRepertoire( this ) )
 		{
-			this.largeurMax    = this.panelPrincipal.getLargeurMax(this);
-			
+			this.largeurMax = this.panelPrincipal.getLargeurMax( this );
+
 			if ( this.estClique )
 			{
-				this.hauteurMax = this.panelPrincipal.getTaille( this , true );
+				this.hauteurMax = this.panelPrincipal.getTaille( this, true );
 			}
 			else
 			{
-				this.hauteurMax = this.panelPrincipal.getTaille( this , false );
+				this.hauteurMax = this.panelPrincipal.getTaille( this, false );
 			}
 		}
 		else
 		{
-			this.largeurMax    = 10 * this.nomClasse.length();
+			this.largeurMax = 10 * this.nomClasse.length();
 			this.hauteurMax = 100;
 		}
 
-		// Appliquer taille
+		/* Application de la taille */
 		this.setSize( this.largeurMax, this.hauteurMax );
-		this.setPreferredSize( getSize() );
+		this.setPreferredSize( this.getSize() );
 		this.repaint();
 	}
 
-    protected void paintComponent(Graphics g)
+	/**
+	* Dessine le bloc avec son contenu (nom, attributs, méthodes).
+	* @param g contexte graphique
+	*/
+	protected void paintComponent( Graphics g )
 	{
-		super.paintComponent(g);
+		super.paintComponent( g );
 
-		Graphics2D g2 = (Graphics2D) g;
-		g2.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING, java.awt.RenderingHints.VALUE_ANTIALIAS_ON);
+	
+		FontMetrics fm;
+		Graphics2D  g2;
+		String      stereoType;
+		int         yCourant;
+		int         margeHorizontale;
+		int         margeVerticalNom;
+		int         margeVerticalAttributs;
+		int         hauteurLigneAttribut;
+		int         hauteurLigneMethode;
+		int         hauteurNom;
+		int         hauteurAttributs;
+		int         yTexte;
+		int         xStereotype;
+		int         xNomClasse;
 
-		int yCourant = 0;
+		g2 = (Graphics2D) g;
+		g2.setRenderingHint( java.awt.RenderingHints.KEY_ANTIALIASING,
+							java.awt.RenderingHints.VALUE_ANTIALIAS_ON );
 
-		int margeHorizontale       = 15;
-		int margeVerticalNom       = 40;
-		int margeVerticalAttributs = 20;
-		int hauteurLigneAttribut   = 18;
-		int hauteurLigneMethode    = 18;
+		yCourant               = 0;
+		margeHorizontale       = 15;
+		margeVerticalNom       = 40;
+		margeVerticalAttributs = 20;
+		hauteurLigneAttribut   = 18;
+		hauteurLigneMethode    = 18;
 
-		/*---------------------------------------------*/
-		/*on calcul la taille des différents composants*/
-		/*---------------------------------------------*/
-
-		int hauteurNom       = margeVerticalNom;
-
-		int hauteurAttributs = margeVerticalAttributs;
+		/*------------------------------------------*/
+		/* Calcul des hauteurs des composants       */
+		/*------------------------------------------*/
+		
+		hauteurNom       = margeVerticalNom;
+		hauteurAttributs = margeVerticalAttributs;
 
 		if ( this.panelPrincipal.getListeAttributs( this ) != null )
 		{
-			hauteurAttributs += this.panelPrincipal.getListeAttributs( this ).size() * hauteurLigneAttribut;
-		}
-		else
-		{
-			hauteurAttributs += 0 * hauteurLigneAttribut;
+			hauteurAttributs += this.panelPrincipal.getListeAttributs( this ).size() * 
+								hauteurLigneAttribut;
 		}
 
-		// Contour global
+		/* Dessin du contour global */
 		if ( this.panelPrincipal.nomEstDansRepertoire( this ) )
 		{
-			g2.setColor ( Color.BLACK );
+			g2.setColor( Color.BLACK );
 		}
 		else
 		{
@@ -120,230 +168,292 @@ public class Bloc extends JPanel
 		}
 
 		g2.setStroke( new BasicStroke( 1.0f ) );
-		g2.drawRect  ( 0, 0, this.largeurMax - 1, this.hauteurMax - 1 );
+		g2.drawRect( 0, 0, this.largeurMax - 1, this.hauteurMax - 1 );
 
-		// Nom de la classe
+		/*------------------------------------------*/
+		/* Dessin du nom de la classe               */
+		/*------------------------------------------*/
+		
 		g2.setFont( new Font( "Arial", Font.BOLD, 12 ) );
-		FontMetrics fm     = g2.getFontMetrics();
-		int         yTexte = 20;
+		fm     = g2.getFontMetrics();
+		yTexte = 20;
 
-
-		/*-------------------------------*/
-		/*on dessine les noms des classes*/
-		/*-------------------------------*/
-
-		String stereoType = "";
-		int xStereotype;
+		stereoType = "";
+		
 		if ( this.panelPrincipal.getMotCle( this ) == null )
 		{
-			stereoType = "<< interface >>";
-			xStereotype   = ( this.largeurMax - fm.stringWidth(stereoType)) / 2;
-			g2.drawString(stereoType, xStereotype, yTexte);
+			stereoType  = "<< interface >>";
+			xStereotype = ( this.largeurMax - fm.stringWidth( stereoType ) ) / 2;
+			g2.drawString( stereoType, xStereotype, yTexte );
 			yTexte += 18;
 		}
 		else
 		{
-			if ( ! this.panelPrincipal.getMotCle( this ).equals( "class" ) )
+			if ( !this.panelPrincipal.getMotCle( this ).equals( "class" ) )
 			{
-				stereoType = "<< " + this.panelPrincipal.getMotCle( this ) + " >>";
-				xStereotype   = ( this.largeurMax - fm.stringWidth(stereoType)) / 2;
-				g2.drawString(stereoType, xStereotype, yTexte);
+				stereoType  = "<< " + this.panelPrincipal.getMotCle( this ) + " >>";
+				xStereotype = ( this.largeurMax - fm.stringWidth( stereoType ) ) / 2;
+				g2.drawString( stereoType, xStereotype, yTexte );
 				yTexte += 18;
 			}
 		}
 
-		int    xNomClasse  = ( this.largeurMax - fm.stringWidth( this.nomClasse ) ) / 2;
+		xNomClasse = ( this.largeurMax - fm.stringWidth( this.nomClasse ) ) / 2;
 		g2.drawString( this.nomClasse, xNomClasse, yTexte );
 
-
-		// Ligne séparatrice nom classe/attributs
+		/* Ligne séparatrice nom/attributs */
 		yCourant = hauteurNom;
-		g2.drawLine(0, yCourant, this.largeurMax, yCourant);
+		g2.drawLine( 0, yCourant, this.largeurMax, yCourant );
 
-
-		/*------------------------*/
-		/*on dessine les attributs*/
-		/*------------------------*/
-
+		/*------------------------------------------*/
+		/* Dessin des attributs                     */
+		/*------------------------------------------*/
+		
 		g2.setFont( new Font( "Monospaced", Font.PLAIN, 10 ) );
-
 		yTexte = yCourant + hauteurLigneAttribut;
 
-		//on parcours tous les attributs si on veux l'affichage complet
-		if( this.estClique )
+		if ( this.estClique )
 		{
-			if ( this.panelPrincipal.getListeAttributs( this ) != null )
-			{
-				for ( Attribut a : this.panelPrincipal.getListeAttributs( this ) )
-				{
-					String affichage = this.panelPrincipal.afficherAttribut( a );
-	
-					g2.drawString( this.panelPrincipal.afficherAttribut( a ), margeHorizontale / 2, yTexte);
-	
-					if ( a.isStatic() )
-					{
-						int largeurTexte = g2.getFontMetrics().stringWidth( affichage );
-	
-						g2.drawLine( margeHorizontale / 2, yTexte + 1, margeHorizontale / 2 + largeurTexte, yTexte + 1 );
-					}
-	
-					yTexte += hauteurLigneAttribut;
-				}
-			}  
+			this.dessinerAttributsComplet( g2, yTexte, hauteurLigneAttribut, 
+											margeHorizontale );
 		}
 		else
 		{
-			int cpt = 0;
-
-			if ( this.panelPrincipal.getListeAttributs( this ) != null )
-			{
-				//utilise un compteur pour parcourir les trois premiers attributs
-				for ( Attribut a : this.panelPrincipal.getListeAttributs( this ) )
-				{
-					if(cpt > 2)
-						break;
-	
-					cpt++;
-	
-					String affichage = this.panelPrincipal.afficherAttribut( a );
-	
-					g2.drawString( affichage, margeHorizontale / 2, yTexte);
-	
-					if ( a.isStatic() )
-					{
-						int largeurTexte = g2.getFontMetrics().stringWidth( affichage );
-	
-						g2.drawLine( margeHorizontale / 2, yTexte + 1, margeHorizontale / 2 + largeurTexte, yTexte + 1 );
-					}
-	
-					yTexte += hauteurLigneAttribut;
-				}
-
-				//rajout des points de suspension s'il y a plus de trois attributs à cacher
-				if(cpt > 2)
-				{
-					g2.drawString( "...", margeHorizontale / 2, yTexte);
-					yTexte += hauteurLigneAttribut;
-				}
-			}
-
+			this.dessinerAttributsReduit( g2, yTexte, hauteurLigneAttribut, 
+										margeHorizontale );
 		}
 
-		// Ligne séparatrice attributs/méthodes
+		/* Ligne séparatrice attributs/méthodes */
 		yCourant += hauteurAttributs;
-		g2.drawLine(0, yCourant, this.largeurMax, yCourant);
+		g2.drawLine( 0, yCourant, this.largeurMax, yCourant );
 
-
-		/*-----------------------*/
-		/*on dessine les méthodes*/
-		/*-----------------------*/
-
+		/*------------------------------------------*/
+		/* Dessin des méthodes                      */
+		/*------------------------------------------*/
+		
 		yTexte = yCourant + 16;
 
-		if(this.estClique)
+		if ( this.estClique )
 		{
-			if ( this.panelPrincipal.getListeMethodes( this ) != null )
-			{
-				for ( Methode m : this.panelPrincipal.getListeMethodes( this ) )
-				{
-					String affichage = this.panelPrincipal.afficherMethode( m, true );
-	
-					g2.drawString( affichage, margeHorizontale / 2, yTexte );
-	
-					if ( m.isStatic() )
-					{
-						int largeurTexte = g2.getFontMetrics().stringWidth( affichage );
-	
-						g2.drawLine( margeHorizontale / 2, yTexte + 1, margeHorizontale / 2 + largeurTexte, yTexte + 1 );
-					}
-	
-					yTexte += hauteurLigneMethode;
-				}
-			}
+			this.dessinerMethodesComplet( g2, yTexte, hauteurLigneMethode, 
+										margeHorizontale );
 		}
 		else
 		{
-			int cpt = 0;
+			this.dessinerMethodesReduit( g2, yTexte, hauteurLigneMethode, 
+										margeHorizontale );
+		}
+	}
 
-			if ( this.panelPrincipal.getListeMethodes( this ) != null )
+	/**
+	* Dessine tous les attributs de la classe.
+	*/
+	private void dessinerAttributsComplet( Graphics2D g2, int yTexte, 
+											int hauteurLigne, int marge )
+	{
+		String affichage   ;
+		int    largeurTexte;
+
+		if ( this.panelPrincipal.getListeAttributs( this ) != null )
+		{
+			for ( Attribut a : this.panelPrincipal.getListeAttributs( this ) )
 			{
-				//utilise un compteur pour parcourir seulement les trois premières méthodes
-				for ( Methode m : this.panelPrincipal.getListeMethodes( this ) )
+				affichage = this.panelPrincipal.afficherAttribut( a );
+				g2.drawString( affichage, marge / 2, yTexte );
+
+				/* Soulignement pour attributs static */
+				if ( a.isStatic() )
 				{
-					if(cpt > 2)
-						break;
-	
-					cpt++;
-	
-					String affichage = this.panelPrincipal.afficherMethode( m, false );
-	
-					g2.drawString( affichage, margeHorizontale / 2, yTexte );
-	
-					if ( m.isStatic() )
-					{
-						int largeurTexte = g2.getFontMetrics().stringWidth( affichage );
-	
-						g2.drawLine( margeHorizontale / 2, yTexte + 1, margeHorizontale / 2 + largeurTexte, yTexte + 1 );
-					}
-	
-					yTexte += hauteurLigneMethode;
+				largeurTexte = g2.getFontMetrics().stringWidth( affichage );
+				g2.drawLine( marge / 2, yTexte + 1, marge / 2 + largeurTexte, 
+							yTexte + 1 );
 				}
-	
-				//rajout de points de suspension s'il y a plus de trois méthodes à cacher
-				if(cpt > 2)
-				{
-					g2.drawString( "...", margeHorizontale / 2, yTexte);
-					yTexte += hauteurLigneAttribut;
-				}
+
+				yTexte += hauteurLigne;
 			}
 		}
 	}
 
-    // --- Classe interne privée pour drag ---
-    private class GereSouris extends MouseAdapter
-    {
-        private Point coordonneePoint;
+	/**
+	* Dessine les 3 premiers attributs avec ellipse si nécessaire.
+	*/
+	private void dessinerAttributsReduit( Graphics2D g2, int yTexte, 
+											int hauteurLigne, int marge )
+	{
+		String affichage        ;
+		int    cpt, largeurTexte;
 
-        public void mousePressed ( MouseEvent e )
-        {
-            coordonneePoint = e.getPoint ( );
+		cpt = 0;
 
-			//partie pour gérer le clic droit
-			if ( e.getButton() == 3 )
+		if ( this.panelPrincipal.getListeAttributs( this ) != null )
+		{
+			/* Parcours des 3 premiers attributs */
+			for ( Attribut a : this.panelPrincipal.getListeAttributs( this ) )
 			{
-				estClique = true;
-				repaint();
+				if ( cpt > 2 )
+				break;
+
+				cpt++;
+
+				affichage = this.panelPrincipal.afficherAttribut( a );
+				g2.drawString( affichage, marge / 2, yTexte );
+
+				/* Soulignement pour attributs static */
+				if ( a.isStatic() )
+				{
+				largeurTexte = g2.getFontMetrics().stringWidth( affichage );
+				g2.drawLine( marge / 2, yTexte + 1, marge / 2 + largeurTexte, 
+							yTexte + 1 );
+				}
+
+				yTexte += hauteurLigne;
 			}
-        }
 
-        public void mouseDragged ( MouseEvent e )
-		{
-			if ( estClique ) { return; }
-
-			int dx = e.getX() - coordonneePoint.x;
-			int dy = e.getY() - coordonneePoint.y;
-
-			int nouveauX = getX() + dx;
-			int nouveauY = getY() + dy;
-
-			// blocage des coordonnées négatives
-			if ( nouveauX < 0 ) nouveauX = 0;
-			if ( nouveauY < 0 ) nouveauY = 0;
-
-			// déplacement visuel
-			setLocation( nouveauX, nouveauY );
-
-			// sauvegarde côté modèle
-			Bloc.this.panelPrincipal.setPosition( Bloc.this, nouveauX, nouveauY );
-
-			// mise à jour flèches
-			getParent().repaint();
+			/* Ajout des points de suspension si nécessaire */
+			if ( cpt > 2 )
+			{
+				g2.drawString( "...", marge / 2, yTexte );
+			}
 		}
+	}
 
-		public void mouseReleased(MouseEvent e)
+	/**
+		* Dessine toutes les méthodes de la classe.
+		*/
+	private void dessinerMethodesComplet( Graphics2D g2, int yTexte, 
+										int hauteurLigne, int marge )
+	{
+		String affichage;
+		int    largeurTexte;
+
+		if ( this.panelPrincipal.getListeMethodes( this ) != null )
 		{
-			estClique = false;
+			for ( Methode m : this.panelPrincipal.getListeMethodes( this ) )
+			{
+				affichage = this.panelPrincipal.afficherMethode( m, true );
+				g2.drawString( affichage, marge / 2, yTexte );
+
+				/* Soulignement pour méthodes static */
+				if ( m.isStatic() )
+				{
+				largeurTexte = g2.getFontMetrics().stringWidth( affichage );
+				g2.drawLine( marge / 2, yTexte + 1, marge / 2 + largeurTexte, 
+							yTexte + 1 );
+				}
+
+				yTexte += hauteurLigne;
+			}
+		}
+	}
+
+	/**
+	* Dessine les 3 premières méthodes avec ellipse si nécessaire.
+	*/
+	private void dessinerMethodesReduit( Graphics2D g2,   int yTexte, 
+										int hauteurLigne, int marge )
+	{
+		String affichage;
+		int    cpt;
+		int    largeurTexte;
+
+		cpt = 0;
+
+		if ( this.panelPrincipal.getListeMethodes( this ) != null )
+		{
+			/* Parcours des 3 premières méthodes */
+			for ( Methode m : this.panelPrincipal.getListeMethodes( this ) )
+			{
+				if ( cpt > 2 )
+				break;
+
+				cpt++;
+
+				affichage = this.panelPrincipal.afficherMethode( m, false );
+				g2.drawString( affichage, marge / 2, yTexte );
+
+				/* Soulignement pour méthodes static */
+				if ( m.isStatic() )
+				{
+				largeurTexte = g2.getFontMetrics().stringWidth( affichage );
+				g2.drawLine( marge / 2, yTexte + 1, marge / 2 + largeurTexte, 
+							yTexte + 1 );
+				}
+
+				yTexte += hauteurLigne;
+			}
+
+			/* Ajout des points de suspension si nécessaire */
+			if ( cpt > 2 )
+			{
+				g2.drawString( "...", marge / 2, yTexte );
+			}
+		}
+	}
+
+/*--------------------------------------------------------------*/
+/* Classe interne privée : gestion de la souris                 */
+/*--------------------------------------------------------------*/
+
+/**
+	* Gère les événements souris pour le drag-and-drop et le clic droit.
+	*/
+private class GereSouris extends MouseAdapter
+{
+	private Point coordonneePoint;
+
+	/**
+	 * Gère le clic souris (clic droit pour affichage complet).
+	 */
+	public void mousePressed( MouseEvent e )
+	{
+		this.coordonneePoint = e.getPoint();
+
+		/* Gestion du clic droit */
+		if ( e.getButton() == 3 )
+		{
+			estClique = true;
 			repaint();
 		}
-    }
+	}
+
+	/**
+	 * Gère le déplacement du bloc par drag-and-drop.
+	 */
+	public void mouseDragged( MouseEvent e )
+	{
+		int dx, dy;
+		int nouveauX, nouveauY;
+
+		if ( estClique )
+			return;
+
+		dx = e.getX() - this.coordonneePoint.x;
+		dy = e.getY() - this.coordonneePoint.y;
+
+		nouveauX = getX() + dx;
+		nouveauY = getY() + dy;
+
+		/* Blocage des coordonnées négatives */
+		if ( nouveauX < 0 ) nouveauX = 0;
+		if ( nouveauY < 0 ) nouveauY = 0;
+
+		/* Déplacement visuel */
+		setLocation( nouveauX, nouveauY );
+
+		/* Sauvegarde côté modèle */
+		Bloc.this.panelPrincipal.setPosition( Bloc.this, nouveauX, nouveauY );
+
+		/* Mise à jour des flèches */
+		getParent().repaint();
+	}
+
+	/**
+	 * Gère le relâchement de la souris.
+	 */
+	public void mouseReleased( MouseEvent e )
+	{
+		estClique = false;
+		repaint();
+	}
+}
 }
